7facb8370f9db2b3cd74925c0141a0f9c970bc55
dintherio <red.dot_curse@proton.me>
2026-09-09 17:20:11 +0200

    rollback okay, added checklist

--- conduit-backend/conduit/settings.py
+++ conduit-backend/conduit/settings.py
@@ -16,7 +16,7 @@ import environ
 env = environ.Env()
 
 # Rollback test
-raise RuntimeError("Simulating production deployment crash for rollback test")
+# raise RuntimeError("Simulating production deployment crash for rollback test")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
